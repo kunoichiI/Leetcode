309dbf4206c6db49f69941102c900577deff89f3
AlwaysTPW <mingyuanwang99@gmail.com>
2016-04-14 22:38:19 -0700

    Initial commit:reverse linked list

---
+++ src/leetcode/ReverseSinglyLinkedlist.java
@@ -0,0 +1,44 @@
+package leetcode;
+
+class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) {val = x; next = null;}
+	
+}
+public class ReverseSinglyLinkedlist {
+	
+	
+	public static ListNode reverseList(ListNode head) {
+		if (head == null || head.next == null) return head;
+		ListNode second = head.next;
+		head.next = null;
+		ListNode reverseRest = reverseList(second);
+		second.next = head;
+		return reverseRest;
+	}
+	
+	
+	public static void main(String[] args) {
+		ListNode n1 = new ListNode(2);
+		ListNode n2 = new ListNode(3);
+		ListNode n3 = new ListNode(4);
+ 
+		ListNode n4 = new ListNode(3);
+		ListNode n5 = new ListNode(4);
+		ListNode n6 = new ListNode(5);
+ 
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+
+		ListNode result = reverseList(n1);
+		while(result != null) {
+			System.out.print(result.val);
+			result = result.next;
+		}
+	}
+
+}
