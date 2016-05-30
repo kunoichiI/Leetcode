3bace12f8d87dc24a93d051ed8738db36e3173bc
AlwaysTPW <mingyuanwang99@gmail.com>
2016-05-30 16:36:28 -0500

    Merge K sorted lists

---
+++ src/leetcode/MergeKSortedLists.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.PriorityQueue;
+import java.util.Comparator;
+
+//class ListNode {
+//	int val;
+//	ListNode next;
+//	ListNode(int x) {val = x;}
+//}
+
+public class MergeKSortedLists {
+	
+	public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
+            public int compare(ListNode n1, ListNode n2) {
+                return n1.val - n2.val;
+            }
+        });
+        
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        
+        for (ListNode list: lists) {
+            if (list!= null) {
+                queue.offer(list);
+            }
+        }
+        while(!queue.isEmpty()) {
+            ListNode n = queue.poll();
+            p.next = n;
+            p = p.next;
+            if (n.next != null) {
+                queue.offer(n.next);
+            }
+        }
+        return head.next;
+    }
+}
