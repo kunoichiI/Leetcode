ab8e0f364e52126ae2e8c72ea5ddbc59d2fe2ec9
AlwaysTPW <mingyuanwang99@gmail.com>
2016-09-05 17:16:18 -0700

    fix bugs

--- src/leetcode/Word_Search_II.java
+++ src/leetcode/Word_Search_II.java
@@ -62,79 +62,82 @@ public class Word_Search_II {
 		
 		visited[x][y] = false;
 	}
+	private class TrieNode {
+	    TrieNode[] arr;
+	    boolean isEnd;
+	    
+	    public TrieNode() {
+	    	this.arr = new TrieNode[26];
+	    }
+	}
+	
+	private class Trie {
+	    private TrieNode root;
+	    
+	    public Trie() {
+	        root = new TrieNode();
+	    }
+	    
+	    // Insert a word into a trie
+	    public void insert(String word) {
+	    	TrieNode p = root;
+	    	for (int i = 0; i < word.length(); i++) {
+	    		char c = word.charAt(i);
+	    		int index = c - 'a';
+	    		if (p.arr[index] == null) {
+	    			TrieNode temp = new TrieNode();
+	    			p.arr[index] = temp;
+	    			p = temp;
+	    		}else {
+	    			p = p.arr[index];
+	    		}
+	    	}
+	    }
+	    
+	    
+	    
+	    // Return if the word is in the trie
+	    public boolean search(String word) {
+	    	TrieNode p = searchNode(word);
+	    	if (p == null) {
+	    		return false;
+	    	}else {
+	    		if (p.isEnd) {
+	    			return true;
+	    		}
+	    		return false;
+	    	}
+	    }
+	    
+	    // Returns if there is any word in the trie
+	    // that starts with the given prefix.
+	    public boolean startsWith(String prefix) {
+	    	TrieNode p = searchNode(prefix);
+	    	if (p == null) {
+	    		return false;
+	    	}else 
+	    		return true;
+	    }
+	    
+	    public TrieNode searchNode(String s) {
+	    	TrieNode p = root;
+	    	for (int i = 0; i < s.length(); i++) {
+	    		char c = s.charAt(i);
+	    		int index = c - 'a';
+	    		if (p.arr[index] == null) {
+	    			return null;
+	    		}else {
+	    			p = p.arr[index];
+	    		}
+	    	}
+	    	if (p == root) {
+	    		return null;
+	    	}
+	    	return p;
+	    }
 }
 
-class TrieNode {
-    TrieNode[] arr;
-    boolean isEnd;
-    
-    public TrieNode() {
-    	this.arr = new TrieNode[26];
-    }
-}
 
-class Trie {
-    private TrieNode root;
-    
-    public Trie() {
-        root = new TrieNode();
-    }
-    
-    // Insert a word into a trie
-    public void insert(String word) {
-    	TrieNode p = root;
-    	for (int i = 0; i < word.length(); i++) {
-    		char c = word.charAt(i);
-    		int index = c - 'a';
-    		if (p.arr[index] == null) {
-    			TrieNode temp = new TrieNode();
-    			p.arr[index] = temp;
-    			p = temp;
-    		}else {
-    			p = p.arr[index];
-    		}
-    	}
-    }
-    
-    
-    
-    // Return if the word is in the trie
-    public boolean search(String word) {
-    	TrieNode p = searchNode(word);
-    	if (p == null) {
-    		return false;
-    	}else {
-    		if (p.isEnd) {
-    			return true;
-    		}
-    		return false;
-    	}
-    }
-    
-    // Returns if there is any word in the trie
-    // that starts with the given prefix.
-    public boolean startsWith(String prefix) {
-    	TrieNode p = searchNode(prefix);
-    	if (p == null) {
-    		return false;
-    	}else 
-    		return true;
-    }
-    
-    public TrieNode searchNode(String s) {
-    	TrieNode p = root;
-    	for (int i = 0; i < s.length(); i++) {
-    		char c = s.charAt(i);
-    		int index = c - 'a';
-    		if (p.arr[index] == null) {
-    			return null;
-    		}else {
-    			p = p.arr[index];
-    		}
-    	}
-    	if (p == root) {
-    		return null;
-    	}
-    	return p;
-    }
+
+
 }
