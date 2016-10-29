3f41faf4f5b429db61be10e3bff721d1f803a031
AlwaysTPW <mingyuanwang99@gmail.com>
2016-10-29 13:39:53 -0700

    Top k freq numbers - bucket sort solution

--- src/leetcode/TopKFrequentElements.java
+++ src/leetcode/TopKFrequentElements.java
@@ -8,16 +8,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
-class Pair {
-	int num;
-	int count;
-	public Pair(int num, int count) {
-		this.num = num;
-		this.count = count;
-	}
-}
+//class Pair {
+//	int num;
+//	int count;
+//	public Pair(int num, int count) {
+//		this.num = num;
+//		this.count = count;
+//	}
+//}
 
 public class TopKFrequentElements {
+	// Time complexity : O(nlogk), n is the array size
+	/**
 	// Given a non-empty array of integers, return the k most frequent elements.
 	// For example,
 	// Given [1,1,1,2,2,3] and k = 2, return [1,2].
@@ -67,5 +69,55 @@ public class TopKFrequentElements {
 		tkfe.topKFrequent(nums, k);
 
 	}
+	
+	**/
+	
+	public List<Integer> topKFrequent(int[] nums, int k) {
+		// Time complexity: O(n) bucket sort!!
+	    //count the frequency for each element
+	    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    for(int num: nums){
+	        if(map.containsKey(num)){
+	            map.put(num, map.get(num)+1);
+	        }else{
+	            map.put(num, 1);
+	        }
+	    }
+	 
+	    //get the max frequency
+	    int max = 0;
+	    for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+	        max = Math.max(max, entry.getValue());
+	    }
+	 
+	    //initialize an array of ArrayList. index is frequency, value is list of numbers
+	    ArrayList<Integer>[] arr = (ArrayList<Integer>[]) new ArrayList[max+1];
+	    for(int i=1; i<=max; i++){
+	        arr[i]=new ArrayList<Integer>();
+	    }
+	 
+	    for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+	        int count = entry.getValue();
+	        int number = entry.getKey();
+	        arr[count].add(number);
+	    }
+	 
+	    List<Integer> result = new ArrayList<Integer>();
+	 
+	    //add most frequent numbers to result
+	    for(int j=max; j>=1; j--){
+	        if(arr[j].size()>0){
+	            for(int a: arr[j]){
+	                result.add(a);
+	            }
+	        }
+	 
+	        if(result.size()==k)
+	            break;
+	    }
+	 
+	    return result;
+	}
+
 
 }
