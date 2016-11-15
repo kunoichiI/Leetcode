3afaaee2053db264a078875a453a2252b4d330fd
AlwaysTPW <mingyuanwang99@gmail.com>
2016-11-15 11:22:51 -0800

    sort list

---
+++ src/leetcode/ReverseBits.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+//190. Reverse Bits  (Amazon onsite question)
+//Reverse bits of a given 32 bits unsigned integer.
+//
+//For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), 
+//return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+
+public class ReverseBits {
+
+	public static int reverseBits(int n) {
+		int reversed = 0;
+        for (int i = 0; i < 32; i++) {
+            reversed = (reversed << 1) | (n & 1);
+            n = (n >> 1);
+        }
+        return reversed;
+	}
+	
+	public static void main(String[] args) {
+		int n = 43261596;
+		System.out.println(reverseBits(n));
+	}
+
+}
--- src/leetcode/TwoSumIII.java
+++ src/leetcode/TwoSumIII.java
@@ -3,7 +3,7 @@ package leetcode;
 import java.util.HashMap;
 import java.util.Map;
 
-//170. Two Sum III - Data structure design Total Accepted: 311 Total Submissions: 1345
+//170. Two Sum III 
 //Design and implement a TwoSum class. It should support the following operations:add and find.
 //add - Add the number to an internal data structure.
 //find - Find if there exists any pair of numbers which sum is equal to the value.
