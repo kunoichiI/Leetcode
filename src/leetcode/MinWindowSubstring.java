1838b4ce7d82d6b1902fe3c844b709e612c82f89
mingyuan <mingyuanwang99@gmail.com>
2018-01-31 11:58:26 -0800

    leetcode 76 min window substring

--- src/leetcode/MinWindowSubstring.java
+++ src/leetcode/MinWindowSubstring.java
@@ -1,12 +1,12 @@
 package leetcode;
 
-// 76 Question
-//Given a string source and a string target, find the minimum window in source which will contain all the characters in target.
-//Notice
-//If there is no such window in source that covers all characters in target, return the empty string "".
-//If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in source.
-//Clarification
-//Should the characters in minimum window has the same order in target?
+// 76 Question 和pramp的题目smallest substring of all unique characters 一摸一样，可以用hashmap加headIdx，tailIndex解决此问题。
+//Given a string source and a string target, 
+// find the minimum window in source which will contain all the characters in target.
+//Notice If there is no such window in source that covers all characters in target, return the empty string "".
+//If there are multiple such windows, you are guaranteed that there 
+// will always be only one unique minimum window in source.
+//Clarification Should the characters in minimum window has the same order in target?
 //Not necessary.
 //Example
 //For source = "ADOBECODEBANC", target = "ABC", the minimum window is "BANC"
