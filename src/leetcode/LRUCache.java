9a840ddee5fb91d30621f7e31e6c0ec7e41b5759
mingyuan <mingyuanwang99@gmail.com>
2018-01-31 11:59:32 -0800

    use a hashmap<Integer, DoublyLinkedListNode> to implement LRUCache, head.next is where to evict least used node, tail.prev is where the recent node goes;

--- src/leetcode/LRUCache.java
+++ src/leetcode/LRUCache.java
@@ -7,147 +7,90 @@ import java.util.*;
 //get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
 //set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
 
-public class LRUCache<K, V> extends LinkedHashMap<K, V> {
-//    private HashMap<Integer, DoubleLinkedListNode> map = new HashMap<Integer, DoubleLinkedListNode>();
-//    private DoubleLinkedListNode head;
-//    private DoubleLinkedListNode end;
-//    private int capacity;
-//    private int len;
-//    
-//    public LRUCache(int capacity) {
-//        this.capacity = capacity;
-//        len = 0;
-//    }
-//    
-//    public int get(int key) {
-//        if (map.containsKey(key)) {
-//            DoubleLinkedListNode latest = map.get(key);
-//            removeNode(latest);
-//            setHead(latest);
-//            return latest.val;
-//        }else {
-//            return -1;
-//        }
-//    }
-//    
-//    public void removeNode(DoubleLinkedListNode node) {
-//        DoubleLinkedListNode cur = node;
-//        DoubleLinkedListNode pre = cur.prev;
-//        DoubleLinkedListNode nex = cur.next;
-//        
-//        if (pre != null) {
-//            pre.next = nex;
-//        }else {
-//            head = nex;
-//        }
-//        
-//        if (nex != null) {
-//            nex.prev = pre;
-//        }else {
-//            end = pre;
-//        }
-//    }
-//    
-//    public void setHead(DoubleLinkedListNode node) {
-//        node.next = head;
-//        node.prev = null;
-//        if (head != null) {
-//            head.prev = node;
-//        }
-//        
-//        head = node;
-//        if (end == null) {
-//            end = node;
-//        }
-//    }
-//    
-//    public void set(int key, int value) {
-//        if (map.containsKey(key)) {
-//            DoubleLinkedListNode oldNode = map.get(key);
-//            oldNode.val = value;
-//            removeNode(oldNode);
-//            setHead(oldNode);
-//        }else {
-//            DoubleLinkedListNode node = new DoubleLinkedListNode(key, value);
-//            if (len < capacity) {
-//                setHead(node);
-//                map.put(key, node);
-//                len++;
-//            }else {
-//                map.remove(end.key);
-//                end = end.prev;
-//                if (end != null) {
-//                    end.next = null;
-//                }
-//                setHead(node);
-//                map.put(key, node);
-//            }
-//        }
-//    }
-//    
-//    
-//}
-//
-//class DoubleLinkedListNode {
-//    public int val;
-//    public int key;
-//    public DoubleLinkedListNode prev;
-//    public DoubleLinkedListNode next;
-//    
-//    public DoubleLinkedListNode(int key, int value) {
-//        this.val = value;
-//        this.key = key;
-//    }
-	//利用java本身带的LinkedHashMap， 设定doubleLinkedList的order为access order，这样access最少（即LRU）的node就会在capacity满之后再access的时候被删除。
-	private static final long serialVersionUID = 1L;
-
-	private final int capacity;
-
-	@Override
-	protected boolean removeEldestEntry(java.util.Map.Entry<K, V> eldest) {
-		// Remove the eldest element whenever size of cache exceeds the capacity
-		return (size() > this.capacity);
-	}
-
-	public LRUCache(int capacity) {
-		// Call constructor of LinkedHashMap with accessOrder set to true to
-		// achieve LRU Cache behavior
-		super(capacity + 1, 1.0f, true);
-		this.capacity = capacity;
-	}
-
-	/**
-	 * Returns the value corresponding to input key from Cache Map.
-	 * 
-	 * @param key
-	 *            Key for the element whose value needs to be returned
-	 * @return Value of the element with input key or null if no such element
-	 *         exists
-	 */
-	public V find(K key) {
-		return super.get(key);
-	}
-
-	/**
-	 * Set the element with input key and value in the cache. If the element
-	 * already exits, it updates its value.
-	 * 
-	 * @param key
-	 *            Key of the element
-	 * @param value
-	 *            Value of the element
-	 */
-	public void set(K key, V value) {
-		super.put(key, value);
-	}
-
-	/**
+public class LRUCache {
+	private HashMap<Integer, DoubleLinkedListNode> map = new HashMap<Integer, DoubleLinkedListNode>();
+    private DoubleLinkedListNode head;
+    private DoubleLinkedListNode end;
+    private int capacity;
+    private int len;
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        len = 0;
+    }
+    
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            DoubleLinkedListNode latest = map.get(key);
+            removeNode(latest);
+            setHead(latest);
+            return latest.val;
+        }else {
+            return -1;
+        }
+    }
+    
+    public void removeNode(DoubleLinkedListNode node) {
+        DoubleLinkedListNode cur = node;
+        DoubleLinkedListNode pre = cur.prev;
+        DoubleLinkedListNode nex = cur.next;
+        
+        if (pre != null) {
+            pre.next = nex;
+        }else {
+            head = nex;
+        }
+        
+        if (nex != null) {
+            nex.prev = pre;
+        }else {
+            end = pre;
+        }
+    }
+    
+    public void setHead(DoubleLinkedListNode node) {
+        node.next = head;
+        node.prev = null;
+        if (head != null) {
+            head.prev = node;
+        }
+        
+        head = node;
+        if (end == null) {
+            end = node;
+        }
+    }
+    
+    public void set(int key, int value) {
+        if (map.containsKey(key)) {
+            DoubleLinkedListNode oldNode = map.get(key);
+            oldNode.val = value;
+            removeNode(oldNode);
+            setHead(oldNode);
+        }else {
+            DoubleLinkedListNode node = new DoubleLinkedListNode(key, value);
+            if (len < capacity) {
+                setHead(node);
+                map.put(key, node);
+                len++;
+            }else {
+                map.remove(end.key);
+                end = end.prev;
+                if (end != null) {
+                    end.next = null;
+                }
+                setHead(node);
+                map.put(key, node);
+            }
+        }
+    }
+    
+    /**
 	 * Entry point for testing LRU Cache.
 	 */
 	public static void main(String[] args) {
 		// Create the cache with capacity 2
-		LRUCache<Integer, Integer> cache = new LRUCache<>(
-				2);
+		LRUCache cache = new LRUCache(2);
 
 		cache.set(2, 1); // Will add an element with key as 2 and value as 1
 		cache.set(3, 2); // Will add an element with key as 3 and value as 2
@@ -158,11 +101,11 @@ public class LRUCache<K, V> extends LinkedHashMap<K, V> {
 		cache.set(4, 3);
 
 		// Since element with key 2 was removed, it will return null
-		System.out.println(cache.find(2));
+		System.out.println(cache.get(2));
 
 		// It will return value 2 and move the element with key 3 to the head.
 		// After this point, element with key 4 will be least recently accessed
-		System.out.println(cache.find(3));
+		System.out.println(cache.get(3));
 
 		// Will add an element with key as 5 and value as 4. Also will remove
 		// the element with key 4 as it was accessed least recently and cache
@@ -170,6 +113,18 @@ public class LRUCache<K, V> extends LinkedHashMap<K, V> {
 		cache.set(5, 4);
 
 		// Since element with key 2 was removed, it will return null
-		System.out.println(cache.find(4));
+		System.out.println(cache.get(4));
 	}
+}
+
+class DoubleLinkedListNode {
+    public int val;
+    public int key;
+    public DoubleLinkedListNode prev;
+    public DoubleLinkedListNode next;
+    
+    public DoubleLinkedListNode(int key, int value) {
+        this.val = value;
+        this.key = key;
+    }	
 }
