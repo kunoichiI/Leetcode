b904774b85c3222fa3f3edb8207db66deda2fd73
mingyuan <mingyuanwang99@gmail.com>
2018-02-04 22:18:17 -0800

    Move zeros self solution.. & remove invalid parentheses

---
+++ src/leetcodeAbove300/RemoveInvalidParentheses.java
@@ -0,0 +1,70 @@
+package leetcodeAbove300;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class RemoveInvalidParentheses {
+	public static List<String> removeInvalidParentheses(String s) {
+		List<String> res = new ArrayList<>();
+		Set<String> visited = new HashSet<>();
+		boolean level = false;
+		Queue<String> q = new LinkedList<>();
+		
+		if (s == null || s.length() == 0)
+			return res;
+		q.offer(s);
+		visited.add(s);
+		
+		while (!q.isEmpty()) {
+			String str = q.poll();
+			if (isValidParentheses(str)) {
+				res.add(str);
+				level = true;
+			}
+			if (level)
+				continue;
+			for (int i = 0; i < str.length(); i++) {
+				if (!isParenthesis(str.charAt(i)))
+					continue;
+				String sub = new StringBuilder(str).deleteCharAt(i).toString();
+				if (!visited.contains(sub)){
+					q.offer(sub);
+					visited.add(sub);
+				}
+			}
+		}
+		return res;
+	}
+	
+	private static boolean isParenthesis(char c) {
+		return (c == '(' || c == ')');
+	}
+	
+	private static boolean isValidParentheses(String s) {
+		int cnt = 0;
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == '(')
+				cnt++;
+			else if (c == ')')
+				cnt--;
+			if (cnt < 0)
+				return false;
+		}
+		return cnt == 0;
+	}
+	public static void main(String[] args) {
+		String str1 = "()())()";
+		String str2 = "(a)())()";
+		String str3 = ")(";
+		
+		System.out.println("The result of removing str1 is : " + removeInvalidParentheses(str1));
+		System.out.println("The result of removing str2 is : " + removeInvalidParentheses(str2));
+		System.out.println("The result of removing str3 is : " + removeInvalidParentheses(str3));
+	}
+
+}
