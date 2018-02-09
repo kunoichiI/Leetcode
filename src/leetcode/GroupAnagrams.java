080c40e326947a9af843821e09d51dcccfe5946a
mingyuan <mingyuanwang99@gmail.com>
2018-02-09 13:59:38 -0800

    Group Anagrams

--- src/leetcode/GroupAnagrams.java
+++ src/leetcode/GroupAnagrams.java
@@ -14,12 +14,13 @@ import java.util.*;
 //  ["bat"]
 //]
 
-public class GroupAnagrams {
-	public List<List<String>> groupAnagrams(String[] strs) {
+public class GroupAnagrams { // k is the max string length, O(klogk) for sorting string characters, n for strs length, so time 
+	// complexity is O(nklogk)
+	public static List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         for (String str: strs) {
-            String curHash = getHash(str);
+            String curHash = getHash(str); 
             if (!map.containsKey(curHash)) {
                 map.put(curHash, new ArrayList<>());
             }
@@ -34,9 +35,25 @@ public class GroupAnagrams {
         return res;
     }
     
-    public String getHash(String str) {
+    public static String getHash(String str) {
         char[] strToArr = str.toCharArray();
         Arrays.sort(strToArr);
         return new String(strToArr);
+    }
+    
+    public static void main(String[] args) {
+    		String[] strs = {"ate", "eat", "tea", "tan", "nat", "bat" };
+    		List<List<String>> res = groupAnagrams(strs);
+    		res.stream()
+    			.forEach(System.out::println); // using stream API
+    		System.out.println("-------");
+//    		for (List<String> l : res) { // java 7 way of output an list
+//    			System.out.println(l.toString());
+//    		}
+//    		Map<String, Integer> map = new TreeMap<>();
+//    		map.put("hello", 1);
+//    		map.put("world", 2);
+//    		map.put("apple", 0);
+//    		System.out.println(map);
     }
 }
