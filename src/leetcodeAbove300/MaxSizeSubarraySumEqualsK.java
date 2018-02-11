a1f48c9f81ad2cc9ad6ec219cd46f2b7b3c17f59
mingyuan <mingyuanwang99@gmail.com>
2018-02-11 15:51:14 -0800

    325 Maximum Subarray Size Sum Equals K, use a hashmap to record sum_so_far, if sum_so_far equals K, then return current index plus 1, if map.containsKey(sum_so_far - target), then sum_so_far index - map.get(sum_so_far - target) is the length of longest subarray

--- src/leetcodeAbove300/MaxSizeSubarraySumEqualsK.java
+++ src/leetcodeAbove300/MaxSizeSubarraySumEqualsK.java
@@ -14,25 +14,37 @@ import java.util.*;
 //Follow Up:
 //Can you do it in O(n) time?
 public class MaxSizeSubarraySumEqualsK {
-	public int maxSubArrayLen(int k, int[] nums) {
+	public static int maxSubArrayLen(int k, int[] nums) {
 		if (nums == null || nums.length == 0) return 0;
 		
 		Map<Integer, Integer> map = new HashMap<>();
-		map.put(0, -1);
-		
+		int maxLen = 0;
 		int sum = 0;
-		int maxLen = Integer.MIN_VALUE;
 		for (int i = 0; i < nums.length; i++) {
 			sum += nums[i];
-			if (!map.containsKey(sum)) {
-				map.put(sum, i);
+			if (sum == k) {
+				return Math.max(maxLen, i + 1); /* 
+				sum是截止当前element的和，如果sum == k，
+				则说明这已经是最大长度subarray， index i 加1 即为长度*/
 			}
 			
-			if (map.containsKey(sum - k)) {
-				int index = map.get(sum - k);
-				maxLen = Math.max(i - index, maxLen);
+			int diff = sum - k; /* k = sum - diff, 说明diff index（exclusive）到sum index（inclusive）
+			之间的和等于target */
+			if (map.containsKey(diff)) {
+				maxLen = Math.max(maxLen, i - map.get(diff));
+			}
+			if (!map.containsKey(diff)) {
+				map.put(sum, i); /* 存入每一位index和 its 累积之和*/
 			}
 		}
-		return maxLen == Integer.MIN_VALUE? 0:maxLen;
+		return maxLen;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums1 = { 1, -1, 5, -2, 3 };
+		int[] nums2 = { 1, 3, 5, -2, -1 };
+		System.out.println(maxSubArrayLen(3, nums1)); //=> 4 [1, -1, 5, -2]
+		System.out.println(maxSubArrayLen(3, nums2)); //=> 1 [3]
+		System.out.println(maxSubArrayLen(0, nums2)); // => 0 , cannot find,return max = 0;
 	}
 }
