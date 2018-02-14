7c09c56a9cd19f3e000c3d37c34a7cc0a3d35425
mingyuan <mingyuanwang99@gmail.com>
2018-02-13 19:55:11 -0800

    237 Super tricky delete node in a linked list, you only have access to this node, no head.  So you replace this node value with the one which is next to it

--- src/leetcode/PermutationRecursive.java
+++ src/leetcode/PermutationRecursive.java
@@ -17,13 +17,12 @@ import java.util.List;
 //  [3,1,2],
 //  [3,2,1]
 //]
-public class PermutationRecursive {
+public class PermutationRecursive { // O(n!) time, O(n) space
 	public static List<List<Integer>> permute(int[] nums) {
 		 List<List<Integer>> res = new ArrayList<>();
 	        if (nums == null || nums.length == 0)
 	            return res;
         permute_helper(nums, 0, res);
-        System.out.println("After permutation, the original nums is: " + Arrays.toString(nums));
         return res;
 	}
 	
@@ -34,30 +33,23 @@ public class PermutationRecursive {
                 lst.add(nums[j]);
             }
             res.add(lst);
-            System.out.println("after adding list, the original array is: " + Arrays.toString(nums));
         }
         else {
             for (int j = i; j < nums.length; j++) {
-            		System.out.println("Beginning swap, i is : " + i + " j is : " + j);
                 swap(nums, i, j);
                 permute_helper(nums, i + 1, res);
                 swap(nums, i, j);
-                System.out.println("end swap, i is : " + i + " j is : " + j);
-                System.out.println("end swap, nums is : " + Arrays.toString(nums));
             }
         }	
 	}
 	
 	private static void swap(int[] nums, int i, int j) {
-		//System.out.println("before swap, i is: " + i + " j is: " + j);
 		int tmp = nums[i];
 		nums[i] = nums[j];
 		nums[j] = tmp;
-		//System.out.println("after swap, i is: " + i + " j is: " + j);
 	}
 	public static void main(String[] args) {
 		int[] nums = { 1, 2, 3 };
-		//System.out.println("nums is : " + nums.getClass());
 		List<List<Integer>> res = permute(nums);
 		res.stream().forEach(System.out::println);
 	}
---
+++ src/leetcodeAbove200/DeleteNodeInALinkedList.java
@@ -0,0 +1,15 @@
+package leetcodeAbove200;
+
+import leetcode.ListNode;
+
+//237. Delete Node in a Linked List
+//Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+//
+//Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, 
+//the linked list should become 1 -> 2 -> 4 after calling your function.
+public class DeleteNodeInALinkedList { /* 这个解法简直了。换个值就跟换个头一样。 */
+	public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
