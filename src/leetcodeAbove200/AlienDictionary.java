0ff12862ec125893a17fac26f9e93d3fd9f08f63
mingyuan <mingyuanwang99@gmail.com>
2018-03-10 09:02:01 -0800

    269 Alien Dictionary, use inbound, outbound and a no_pre to record all incoming edges, all outgoing edges, and a set to get current root nodes, every time after removing current root nodes, update inbound and outbound maps to get new root nodes with in-degeee == 0, keep doing this until no more root nodes ;

--- src/leetcodeAbove200/AlienDictionary.java
+++ src/leetcodeAbove200/AlienDictionary.java
@@ -1,69 +1,107 @@
 package leetcodeAbove200;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
-import gfg.Graph;
-
-//269. Given a sorted dictionary of an alien language, find order of characters
-//Examples:
-//
-//Input:  words[] = {"baa", "abcd", "abca", "cab", "cad"}
-//Output: Order of characters is 'b', 'd', 'a', 'c'
-//Note that words are sorted and in the given language "baa" 
-//comes before "abcd", therefore 'b' is before 'a' in output.
-//Similarly we can find other orders.
-//
-//Input:  words[] = {"caa", "aaa", "aab"}
-//Output: Order of characters is 'c', 'a', 'b'
-public class AlienDictionary {
-	private Graph graph;
-	public List<Character> characterOrder(String[] words) {
-		List<Character> res = new LinkedList<>();
-		// corner case
-		if (words == null || words.length == 0)
-			return res;
-		// calculate how many unique characters in words
-		boolean[] chars = new boolean[256];
-		for (int k = 0; k < words.length; k++) {
-			String word = words[k];
-			for (char c : word.toCharArray()) {
-				if (chars[c - 'a'] == false)
-					chars[c - 'a'] = true;
+//269 [Leetcode] Alien Dictionary
+//There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+//Example 1:
+//Given the following words in dictionary,
+//[
+//  "wrt",
+//  "wrf",
+//  "er",
+//  "ett",
+//  "rftt"
+//]
+//The correct order is: "wertf".
+//Example 2:
+//Given the following words in dictionary,
+//[
+//  "z",
+//  "x"
+//]
+//The correct order is: "zx".
+//Example 3:
+//Given the following words in dictionary,
+//[
+//  "z",
+//  "x",
+//  "z"
+//]
+//The order is invalid, so return "".
+//Note:
+//You may assume all letters are in lowercase.
+//You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
+//If the order is invalid, return an empty string.
+//There may be multiple valid order of letters, return any one of them is fine.
+public class AlienDictionary { // O(n) time, O(n) space
+	// reference link: http://fisherlei.blogspot.com/2017/07/leetcode-alien-dictionary-solution.html
+	/* This solution is much shorter than the regular building graph then do topological sort solution! */
+	public static String alienOrder(String[] words) {
+		Map<Character, Set<Character>> inbounds = new HashMap<>();
+		Map<Character, Set<Character>> outbounds = new HashMap<>();
+		Set<Character> no_pre = new HashSet<>();
+		
+		String s = "";
+		for (int i = 0; i < words.length; i++) {
+			String t = words[i];
+			for (char ch : t.toCharArray()) no_pre.add(ch);
+			for (int idx = 0; idx < Math.min(s.length(), t.length()); idx++){
+				char si = s.charAt(idx);
+				char ti = t.charAt(idx);
+				if (si != ti) {
+					if (!inbounds.containsKey(ti))
+						inbounds.put(ti, new HashSet<Character>());
+					inbounds.get(ti).add(si);
+					if (!outbounds.containsKey(si))
+						outbounds.put(si, new HashSet<Character>());
+					outbounds.get(si).add(ti);
+					break;
+				}
+			}
+			s = t;
+		}
+		
+		int vertices = no_pre.size();
+		// Remove all nodes from no_pre which has in-degree larger than 0
+		for (char key: inbounds.keySet()) {
+			if (no_pre.contains(key)) no_pre.remove(key);
+		}
+		
+//		System.out.println("inbounds looks like: " + inbounds);
+//		System.out.println("outbounds looks like: " + outbounds);
+//		System.out.println("no_pre looks like: " + no_pre);
+		
+		String result = "";
+		// start adding nodes which has in-degree equals to 0
+		while (no_pre.size() > 0) {
+			for (char ch: no_pre) {
+				no_pre.remove(ch);
+				result += ch;
+//				System.out.println("result looks like: " + result);
+//				System.out.println("ch is: " + ch);
+//				System.out.println("outbounds has ch: " + outbounds.get(ch));
+				if (outbounds.get(ch) != null) { // this is important, outbounds may not have this vertex!
+					for (char a: outbounds.get(ch)) {
+						inbounds.get(a).remove(ch);
+						if (inbounds.get(a).size() == 0)
+							no_pre.add(a);
+					}
+				}
 			}
 		}
-		int cnt = 0;
-		for (boolean b : chars) {
-			if (b)
-				cnt++;
-		}
-		graph = new Graph(cnt);
-		// gather edges based on words, find the first mismatching character of one pair words
-		for (int i = 0; i < words.length - 1; i++) {
-			String word1 = words[i];
-			String word2 = words[i+1];
-			for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
-				if (word1.charAt(j) != word2.charAt(j))
-					System.out.println("word1 char: " + (word1.charAt(j) - 'a'));
-					System.out.println("word2 char: " + (word2.charAt(j) - 'a'));
-					graph.addEdge(word1.charAt(j) - 'a', word2.charAt(j) - 'a');
-			}
-		}
-		//System.out.println("graph is: " + graph);
-		System.out.println("graph vertice number is: " + graph.getVertices());
-		//System.out.println("graph adjacent list is: " + graph.getAdjList());
-		Arrays.stream(graph.getAdjList())
-			  .forEach(System.out::println);
-		res = graph.topologialSort();
-		return res;
+		
+		return result.length() == vertices ? result : "";
 	}
 	public static void main(String[] args) {
-		AlienDictionary ad = new AlienDictionary();
-		// test cases
-		String[] words1 = {"baa", "abcd", "abca", "cab", "cad"};
-		List<Character> l1 = ad.characterOrder(words1);
-		System.out.println("l1 is: " + l1);
+		String[] words = { "wrt", "wrf", "er", "ett", "rftt" };
+		System.out.println("result is: " + alienOrder(words)); // wertf
+		
+		String[] words1 = { "z", "x", "z" };
+		System.out.println("result is: " + alienOrder(words1)); // ""
+		
+		String[] words2 = { "z", "x" };
+		System.out.println("result is: " + alienOrder(words2)); // "zx"
 	}
 
 }
