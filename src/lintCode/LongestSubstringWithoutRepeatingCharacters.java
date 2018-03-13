9a78ace15b4ec42a16bb205c8e7e744a74f73c66
mingyuan <mingyuanwang99@gmail.com>
2018-03-13 10:03:10 -0700

    384 LintCode Longest Substring Without Repeating Characers, use boolean array to record appearance of characters

---
+++ src/lintCode/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,38 @@
+package lintCode;
+
+public class LongestSubstringWithoutRepeatingCharacters { // o(n) time, o(1) space
+	public static int lengthOfLongestSubstring(String s) {
+	        if (s == null) return 0;
+	        
+	        boolean[] arr = new boolean[256];
+	        int result = 0;
+	        int start = 0;
+	        
+	        for (int i = 0; i < s.length(); i++) {
+	            char c = s.charAt(i);
+	            if (arr[c]) {
+	                result = Math.max(result, i - start);
+	                for (int k = start; k < i; k++) {
+	                    if (s.charAt(k) == c) {
+	                        start = k + 1;
+	                        break;
+	                    }
+	                    arr[k] = false;
+	                }
+	            }
+	            else 
+	                arr[c] = true;
+	        }
+	        result = Math.max(s.length() - start, result);
+	        return result;
+    }
+	public static void main(String[] args) {
+		System.out.println(lengthOfLongestSubstring("z")); // 1
+		System.out.println(lengthOfLongestSubstring("abccab")); // 3
+	}
+	/*
+	 * 解题思路： 用256位布尔数列记录当前字符是否出现，如果布尔值为false，设为true
+	 * 如果布尔值为true，则说明当前位字符是duplicate重复值，所以在这一位更新start起点，把起点前的所有布尔值重新
+	 * 设置为false。计算从start到这一点的长度，与res进行对比。直到循环结束，最后比较一次计算长度和result的值，取较大的那一个
+	 */
+}
