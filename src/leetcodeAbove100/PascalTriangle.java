49366a6ba989f48ca2c2dc237385363d59f28a06
mingyuan <mingyuanwang99@gmail.com>
2018-03-12 18:06:26 -0700

    118 Pascal Triangle

--- src/leetcodeAbove100/PascalTriangle.java
+++ src/leetcodeAbove100/PascalTriangle.java
@@ -1,5 +1,20 @@
-package leetcode;
+package leetcodeAbove100;
+/*
+ * 118. Pascal's Triangle
+ * 
+ * Given numRows, generate the first numRows of Pascal's triangle.
 
+		For example, given numRows = 5,
+		Return
+		
+		[
+		     [1],
+		    [1,1],
+		   [1,2,1],
+		  [1,3,3,1],
+		 [1,4,6,4,1]
+		]
+ */
 import java.util.ArrayList;
 
 public class PascalTriangle {
