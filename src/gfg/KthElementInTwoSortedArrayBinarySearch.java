4c87eff2be0c2f7635ab02932674d80361c063a8
mingyuan <mingyuanwang99@gmail.com>
2018-04-17 16:44:13 -0700

    GFG Kth Element in Two sorted arrays

---
+++ src/gfg/KthElementInTwoSortedArrayBinarySearch.java
@@ -0,0 +1,14 @@
+package gfg;
+
+public class KthElementInTwoSortedArrayBinarySearch {
+	public static int kthElement(int[] arr1, int[] arr2, int k) {
+		// Escape edge cases here.. assume arr1, arr2, k are all valid, 0 <= k <= arr1.length + arr2.length - 1
+		return 1; // not implemented yet, too hard!
+	}
+	public static void main(String[] args) {
+		int[] arr1 = { 2, 3, 6, 7, 9};
+		int[] arr2 = { 1, 4, 8, 10 };
+		System.out.println(kthElement(arr1, arr2, 5)); // 6
+	}
+
+}
