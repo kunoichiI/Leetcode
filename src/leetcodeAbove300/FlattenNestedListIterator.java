4bc3cce519b830488997644020803b87c75f4c55
mingyuan <mingyuanwang99@gmail.com>
2018-04-26 11:44:59 -0700

    LC 341 Flatten Nested List Iterator, clean code

--- src/leetcodeAbove300/FlattenNestedListIterator.java
+++ src/leetcodeAbove300/FlattenNestedListIterator.java
@@ -1,9 +1,9 @@
-package leetcode;
+package leetcodeAbove300;
 
 import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
-
+// 341 Nested List Iterator
 //Given a nested list of integers, implement an iterator to flatten it.
 //
 //Each element is either an integer, or a list -- whose elements may also be integers or other lists.
